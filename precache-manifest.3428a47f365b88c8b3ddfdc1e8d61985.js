a459d3114da450486402cfeb1fe97592b2cca3fd
mihaitopala <topala.mihai34@gmail.com>
2019-12-05 23:33:02 +0200

    Updates

---
+++ precache-manifest.3428a47f365b88c8b3ddfdc1e8d61985.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a8e4d49c57852c6f85f9e3cb7c6ceaf",
+    "url": "/offline.github.io/index.html"
+  },
+  {
+    "revision": "ba1e04954d122ccc97ea",
+    "url": "/offline.github.io/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "59d0bc865eeca0a4a86e",
+    "url": "/offline.github.io/static/js/2.f24cd3f2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/offline.github.io/static/js/2.f24cd3f2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ba1e04954d122ccc97ea",
+    "url": "/offline.github.io/static/js/main.e2a7b18f.chunk.js"
+  },
+  {
+    "revision": "a7575f428f2eb3a04ef7",
+    "url": "/offline.github.io/static/js/runtime-main.cb2b942f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/offline.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
